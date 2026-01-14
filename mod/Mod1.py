69722e4da4e78f7799b57d8506b9c6cf090b97eb
axmed983-gif <axmed983@gmail.com>
2026-01-14 18:55:39 +0200

    tehtävä 1 done

--- mod/Mod1.py
+++ mod/Mod1.py
@@ -2,11 +2,15 @@ print ("Hello World")
 print (" Tämän koodasin itse, jee !" )
 nimi = input ("Mikä on nimesi?")
 print ("Moi " + nimi + ",  mitä kuuluu?")
-print("hyvää /nhuomenta")
+print("hyvää huomenta")
 käyttäjä = input('Anna nimesi: ')
 print("Hauska tavata, " + käyttäjä + "!")
 
-
+import math
+säde = float(input("Anna ympyrän säde: "))
+pinta_ala = math.pi * säde ** 2
+kehan_pituus = 2 * math.pi * säde
+print("Ympyrän pinta-ala on:", pinta_ala)
 
 
 
