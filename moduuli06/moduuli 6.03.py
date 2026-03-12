e82778b6f2ebc4bfa4aa7ec594c132c5a1fc82af
axmed983-gif <axmed983@gmail.com>
2026-03-12 13:06:32 +0200

    tehty

---
+++ moduuli06/moduuli 6.03.py
@@ -0,0 +1,12 @@
+def laske_summa(luvut):
+    luvut=0
+    for luku in luvut:
+        luku += luku
+    return summa
+
+if laske_summa== "main":
+    summa = laske_summa(5)
+    lista = [3,5,7,2]
+    tulos = laske_summa(lista)
+    print("listan lukujen summa on:",tulos)
+
